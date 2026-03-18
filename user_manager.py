6765bd5fa37a594a68865f069efd767836c51438
neul <thinhlee291106@gmail.com>
2026-03-18 22:49:53 +0700

    update

---
+++ user_manager.py
@@ -0,0 +1,7 @@
+from data_manager import DataManager
+from encryption_manager import EncryptionManager
+
+class UserManager:
+    def __init__(self, data_manager: DataManager, encryman):
+        
+        pass
